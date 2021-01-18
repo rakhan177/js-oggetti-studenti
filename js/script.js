f02e13f047b187e3a4287a57b2e7c87ffbde1393
Matteo <metboolean@gmail.com>
2021-01-18 17:48:49 +0100

    finito

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@
 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
 
-// 1° esercizio
+// 1° parte
 
+// dichiarazione variabili:
+var studenti;
+var studenteUtente = {};
+var nome;
+var cognome;
+var eta;
+
+// creo oggetto studente
 var studente = {
   'nome': 'Matteo',
   'cognome': 'Meago',
@@ -12,14 +20,15 @@ var studente = {
 
 console.log('Proprietà studente:');
 
+// ciclo oggetto studente per stampare proprietà
 for(var key in studente){
   console.log('proprietà: ' + key + ', valore: ' + studente[key]);
 }
 
-// 2° esercizio
+// 2° parte
 
+// creo arrayStudenti con oggetti studente
 var arrayStudenti = [
-  // 1° studente
   {
     'nome': 'Luigi',
     'cognome': 'Gennari',
@@ -32,28 +41,54 @@ var arrayStudenti = [
   },
 ];
 
-var studenti;
-
 console.log('Proprietà arrayStudenti:');
 
+// ciclo arrayStudenti
 for(var x = 0; x < arrayStudenti.length; x++){
-  studenti = arrayStudenti[x];
-  for(var key2 in studenti){
-    console.log('proprietà: ' + key2 + ', valore: ' + studenti[key2])
+  // ad ogni incremento apro ciclo for in che stampa proprietà oggetto
+  for(var key2 in arrayStudenti[x]){
+    console.log('proprietà: ' + key2 + ', valore: ' + arrayStudenti[x][key2])
   }
 }
 
-// 3° esercizio
+// 3° parte
 
-var studenteUtente = {};
+// chiedo nome studente e verifico
+nome = prompt('Inserisci nome studente.');
+while(!isNaN(nome)){
+  nome = prompt('Inserisci lettere please ;)...Nome?');
+}
 
-studenteUtente.nome = prompt('Inserisci nome studente.');
-studenteUtente.cognome = prompt('Inserisci cognome studente.');
-studenteUtente.eta = prompt('Inserisci eta studente.');
+// chiedo cognome studente e verifico
+cognome = prompt('Inserisci cognome studente.');
+while(!isNaN(cognome)){
+  cognome = prompt('Inserisci lettere please ;)...Cognome?');
+}
+
+// chiedo eta studente e verifico
+eta = Number(prompt('Inserisci età studente.'));
+while(isNaN(eta) || eta < 6 || eta >= 80){
+  eta = Number(prompt('Inserisci un numero veritiero ;)'));
+}
+
+// assegno i prompt alle proprieta dell' oggetto
+studenteUtente.nome = nome;
+studenteUtente.cognome = cognome;
+studenteUtente.eta = eta;
+
+// pusho il nuovo oggetto nell' arrayStudenti
 arrayStudenti.push(studenteUtente);
 
-console.log('Proprietà studente scelto dall\' utente:');
-
+console.log('Proprietà Studente scelto dall\' utente:');
+// ciclo studente utente e stampo proprietà:
 for(var key3 in studenteUtente){
   console.log('proprietà: ' + key3 + ', valore: ' + studenteUtente[key3])
+}
+
+// ristampo tutti gli oggetti di arrayStudenti
+console.log('arrayStudenti completo:')
+for(var x = 0; x < arrayStudenti.length; x++){
+  for(var key2 in arrayStudenti[x]){
+    console.log('proprietà: ' + key2 + ', valore: ' + arrayStudenti[x][key2])
+  }
 }
